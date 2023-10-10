7a2ceee67978ed9c662b8e44c49f7b28f2675c46
Nxelia <nxelia99@gmail.com>
2023-10-10 12:26:43 +0200

    backend

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import Express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import authRoute from "./Routes/auth.js"
+
+dotenv.config()
+
+const app = Express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin:true
+}
+
+app.get('/', (reg, res)=>{
+    res.send('Api is working')
+})
+
+//database connection
+
+mongoose.set('strictQuery', false)
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+           useNewUrlParser: true,
+           useUnifiedTopology: true 
+        })
+        console.log('MongoDB database is connected.')
+    } catch(err){
+            console.log("MongoDB connection failed.")
+    }
+}
+//middleware
+
+app.use(Express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute)
+
+
+
+app.listen(port, ()=>{
+    connectDB()
+    console.log('Server is running on port ' + port)
+})
