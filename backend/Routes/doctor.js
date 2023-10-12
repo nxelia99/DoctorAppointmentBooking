1b6f40d2b377d95f9d28c7265bfe3d0d95821577
Nxelia <nxelia99@gmail.com>
2023-10-12 18:52:30 +0200

    verify token

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { deleteDoctor, getAllDoctors, getSingleDoctor, updateDoctor } from "../Controllers/doctorController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router()
 
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctors)
-router.put('/:id', updateDoctor)
-router.delete('/:id', deleteDoctor)
+router.put('/:id', updateDoctor, authenticate, restrict(["doctor"]))
+router.delete('/:id', deleteDoctor, authenticate, restrict(["doctor"]))
 
 export default router;
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,12 +1,13 @@
 
 import express from "express";
 import { deleteUser, getAllUsers, getSingleUser, updateUser } from "../Controllers/userController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router()
 
-router.get('/:id', getSingleUser)
-router.get('/', getAllUsers)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.get('/:id', authenticate, restrict(['patient']), getSingleUser)
+router.get('/', getAllUsers, authenticate, restrict(['admin']))
+router.put('/:id', updateUser, authenticate, restrict(['patient']))
+router.delete('/:id', deleteUser, authenticate, restrict(['patient']))
 
 export default router;
